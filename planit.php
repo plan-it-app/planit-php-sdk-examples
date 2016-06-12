b19e03946a6fcdd33328e260b69a761ca5d3d1b6
David Levy <davidlevy@Davids-MacBook-Air.local>
2016-06-12 13:34:34 -0700

    all requires

---
+++ planit.php
@@ -0,0 +1,12 @@
+<?php
+
+requier('/guzzle/src/functions_include.php');
+requier('/psr7/src/functions_include.php');
+requier('/planit-php-sdk/planit.php');
+
+use Planit\API;
+/*
+$x = new \Planit\API(TOKEN, [
+    'ignoreSSL' => false, // default
+    'cookies_jar' => true // default is true, can set a new \GuzzleHttp\Cookie\CookieJar if you want
+]);*/
